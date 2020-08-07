8cf22ffb22c9a669551cc5e861adffff4b3851d7
邝贤发 <578173534@qq.com>
2020-08-07 09:52:32 +0800

    Maven

---
+++ ch02-maven-web/src/main/java/com/ggs/controller/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.ggs.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.print.Printable;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter pw = response.getWriter();
+        pw.println("Hello Maven web");
+        pw.flush();
+        pw.close();
+    }
+}
